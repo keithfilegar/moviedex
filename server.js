8074ff3804cf96ad4458864fbbe604674f23def3
keithfilegar <keith.filegar@gmail.com>
2021-01-22 12:35:23 -0800

    successfully filtering genre on GET /movie

--- server.js
+++ server.js
@@ -11,6 +11,33 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 
+app.use(function validateBearerToken(req, res, next) {
+    const authToken = req.get('Authorization');
+    const apiToken = process.env.API_TOKEN;
+    console.log(`api token is ${apiToken}`);
+    console.log(`auth token is ${authToken}`)
+
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' });
+    }
+
+    next();
+})
+
+function handleGetMovie(req, res) {
+    let response = MOVIEDEX;
+
+    if(req.query.genre) {
+        response = response.filter(movie => 
+            movie.genre.includes(req.query.genre))
+    }
+
+    res.json(response)
+}
+
+app.get('/movie', handleGetMovie);
+
 const PORT = 8000;
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`)
