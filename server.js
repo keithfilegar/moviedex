86d5587f73846215368d24c82c4cace4bb5772ee
keithfilegar <keith.filegar@gmail.com>
2021-01-25 10:42:13 -0800

    prep project for production deployment

--- server.js
+++ server.js
@@ -7,21 +7,18 @@ const MOVIEDEX = require('./moviedex.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization');
     const apiToken = process.env.API_TOKEN;
-    console.log(`api token is ${apiToken}`);
-    console.log(`auth token is ${authToken}`)
-
 
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' });
     }
-
     next();
 })
 
@@ -49,7 +46,16 @@ function handleGetMovie(req, res) {
 
 app.get('/movie', handleGetMovie);
 
-const PORT = 8000;
-app.listen(PORT, () => {
-    console.log(`server is listening on port ${PORT}`)
+app.use((error, req, res, next) => {
+    let response
+    if(process.env.NODE_ENV === 'production') {
+        response = {error: { message: 'server error' }}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
 })
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {})
